cb212963d8e5492ecedaca39ed2139c60682034a
Anthony Roscoe <me@anthonyroscoe.com>
2009-11-21 12:30:57 -0500

    updated library test models to actual models (library, artist, album)

--- apps/library/models.py
+++ apps/library/models.py
@@ -3,15 +3,31 @@ from django.db import models
 class Library(models.Model):
     name = models.CharField(max_length=60, blank=True)
     
+    def __str__(self):
+        return self.name
+    
     class Meta:
         verbose_name_plural = 'libraries'
     
     class Admin:
         pass
 
-class LibraryItem(models.Model):
-    name = models.CharField(max_length=150)
+class Artist(models.Model):
+    name    = models.CharField(max_length=150)
     library = models.ForeignKey(Library)
+
+    def __str__(self):
+        return self.name
     
+    class Admin:
+        pass
+
+class Album(models.Model):
+    name   = models.CharField(max_length=150)
+    artist = models.ForeignKey(Artist)
+
+    def __str__(self):
+        return '%s - %s' % (self.artist.name, self.name)
+
     class Admin:
         pass
