a721a96d0ee8e963e4ef1f506478208335eec820
tjulien <tim.julien@gmail.com>
2009-11-21 15:18:31 -0500

    itunes db import

---
+++ itunes_import.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import plistlib
+from library.models import *
+
+def itunes_import(file, name):
+	itunes = plistlib.readPlist(file)
+	tracks = itunes["Tracks"]
+	library = Library(name=name)
+	library.save()
+	for track in tracks.values():
+		if track.get("Artist") and track.get("Album"):
+			artist,created = library.artist_set.get_or_create(name=track["Artist"])
+			artist.album_set.get_or_create(name=track["Album"])
+
+
+if __name__ == "__main__":
+    itunes_import(sys.argv[1,2])
