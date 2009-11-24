602f4200c4c2983e5dfeaeb70b2fba90e00c0431
Anthony Roscoe <me@anthonyroscoe.com>
2009-11-24 08:28:11 -0500

    fixed some formatting and function names in mb.py

--- apps/library/utils/mb.py
+++ apps/library/utils/mb.py
@@ -27,52 +27,50 @@ def album_diff(library):
     return missing
 
 def same_album(local_album_name, mb_release_group, mb_artist_id):
-        return getReleaseGroupID(local_album_name, mb_artist_id) == mb_release_group.id
+    return get_release_group_id(local_album_name, mb_artist_id) == mb_release_group.id
 
 def _same_album(local_album_name, mb_release_group, mb_artist_id):
-        release_group_filter = ws.ReleaseGroupFilter(
-                                title=local_album_name,
-                                artistId=mb_artist_id,
-                                releaseTypes=(m.Release.TYPE_OFFICIAL,
-                                        m.Release.TYPE_ALBUM))
-        q = ws.Query()
-        releases_with_local_name = q.getReleaseGroups(release_group_filter)
-        if releases_with_local_name:
-                if releases_with_local_name[0].release.asin and mb_release_group.asin:
-                        return mb_release_group.asin == releases_with_local_name[0].releaseGroup.asin
-                else:
-                        return mb_release_group.title == local_album_name
-                # TODO when releaseGroup ids are populated
-                # return mb_release_group.id == releases_with_local_name[0].releaseGroup.id
-        return False
+    release_group_filter = ws.ReleaseGroupFilter(
+                            title=local_album_name,
+                            artistId=mb_artist_id,
+                            releaseTypes=(m.Release.TYPE_OFFICIAL,
+                                    m.Release.TYPE_ALBUM))
+    q = ws.Query()
+    releases_with_local_name = q.getReleaseGroups(release_group_filter)
+    if releases_with_local_name:
+            if releases_with_local_name[0].release.asin and mb_release_group.asin:
+                    return mb_release_group.asin == releases_with_local_name[0].releaseGroup.asin
+            else:
+                    return mb_release_group.title == local_album_name
+            # TODO when releaseGroup ids are populated
+            # return mb_release_group.id == releases_with_local_name[0].releaseGroup.id
+    return False
                 
-def getArtistID(artist_name):
-        name_filter = ws.ArtistFilter(name=artist_name, limit=5)
-        q = ws.Query()
-        return q.getArtists(name_filter)[0].artist.id
+def get_artist_id(artist_name):
+    name_filter = ws.ArtistFilter(name=artist_name, limit=5)
+    q = ws.Query()
+    return q.getArtists(name_filter)[0].artist.id
 
-def getReleaseGroupID(album_name, mb_artist_id):
-        releases = getReleases(album_name, mb_artist_id)
-        if releases:
-                includes = ws.ReleaseIncludes(releaseGroup=True)
-                q = ws.Query()
-                return q.getReleaseById(releases[0].release.id, includes).releaseGroup.id
-        #release_group_filter = ws.ReleaseGroupFilter(
-        #                        title=album_name,
-        #                        artistId=mb_artist_id,
-        #                        releaseTypes=(m.Release.TYPE_OFFICIAL,
-        #                                m.Release.TYPE_ALBUM))
-        #q = ws.Query()
-        #return q.getReleaseGroups(release_group_filter)
-        return None
-        
-def getReleases(name, mb_artist_id):
-        release_filter = ws.ReleaseFilter(
-                                title=name,
-                                artistId=mb_artist_id,
-                                releaseTypes=(m.Release.TYPE_OFFICIAL,
-                                        m.Release.TYPE_ALBUM))
-        q = ws.Query()
-        return q.getReleases(release_filter)
-        
-    
+def get_release_group_id(album_name, mb_artist_id):
+    releases = get_releases(album_name, mb_artist_id)
+    if releases:
+            includes = ws.ReleaseIncludes(releaseGroup=True)
+            q = ws.Query()
+            return q.getReleaseById(releases[0].release.id, includes).releaseGroup.id
+    #release_group_filter = ws.ReleaseGroupFilter(
+    #                        title=album_name,
+    #                        artistId=mb_artist_id,
+    #                        releaseTypes=(m.Release.TYPE_OFFICIAL,
+    #                                m.Release.TYPE_ALBUM))
+    #q = ws.Query()
+    #return q.getReleaseGroups(release_group_filter)
+    return None
+
+def get_releases(name, mb_artist_id):
+    release_filter = ws.ReleaseFilter(
+                            title=name,
+                            artistId=mb_artist_id,
+                            releaseTypes=(m.Release.TYPE_OFFICIAL,
+                                    m.Release.TYPE_ALBUM))
+    q = ws.Query()
+    return q.get_releases(release_filter)
