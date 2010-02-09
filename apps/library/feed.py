8bd0de43715c44bb3ffae53d786cd0117159a6ed
Felix Berger <bflat1@gmx.net>
2010-02-08 22:44:49 -0500

    add amazon search

--- apps/library/feed.py
+++ apps/library/feed.py
@@ -4,7 +4,12 @@ from django.contrib.syndication.feeds import Feed
 from library.models import *
 from datetime import datetime
 
+from settings import AMAZON_KEY, AMAZON_SECRET
+from amazonproduct import API
+
 class NewAlbums(Feed):
+
+    api = API(AMAZON_KEY, AMAZON_SECRET)
 
     def get_object(self, bits):
         if len(bits) != 1:
@@ -27,7 +32,24 @@ class NewAlbums(Feed):
         return obj.missing_albums()[:10]
 
     def item_link(self, obj):
-        return "%s.html" % obj.mb_id
+        amazon_link = search_on_amazon(obj.name, obj.artist.name)
+        if amazon_link:
+            return amazon_link
+        else:
+            return "%s.html" % obj.mb_id
 
     def item_pubdate(self, obj):
         return datetime(obj.release_date.year, obj.release_date.month, obj.release_date.day)
+
+
+    def search_on_amazon(album, artist):
+        try:
+            node = api.item_search('MP3Downloads', Keywords=album + ' ' + artist)
+            for item in node.Items:
+                attributes = item.Item.ItemAttributes
+                if attributes.Creator == artist and attributes.Title == album
+                and attributes.ProductGroup == 'Digital Music Album':
+                return item.Item.DeatailPageURL
+        except:
+            return None
+        
