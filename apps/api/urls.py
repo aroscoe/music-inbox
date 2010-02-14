19e31491c7a89cffa6d83b3193ebf2e3a65b1ef8
Anthony Roscoe <me@anthonyroscoe.com>
2010-02-13 20:16:12 -0500

    cleaned up urls.py in api app to get an idea of what does what

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -8,9 +8,14 @@ library = LibraryResource(permitted_methods=('GET','POST'))
 feeds = {'newalbums': NewAlbums}
 
 urlpatterns = patterns('',
+    # Example of using django_restapi
     url(r'^test/(.*?)/?$', libraries_resource),
-    url(r'^upload/?$', library.create),
-    url(r'^(\d+)/missing/?$', missing),
+    
+    # Library
+    url(r'^library/upload/?$', library.create),
+    url(r'^library/(\d+)/missing/?$', missing),
+    url(r'^library/(.*?)/?$', library),
+    
+    # RSS Feed
     url(r'^feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
-    url(r'^(.*?)/?$', library),
 )
