d8db11be89a8053ff7b1612511b4414c11f41b8b
Felix Berger <bflat1@gmx.net>
2010-02-17 22:09:20 -0500

    started drafting privacy policy

--- apps/library/urls.py
+++ apps/library/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('library.views',
     url(r'^$', 'upload', name="library_home"),
+    (r'^privacy/$', direct_to_template,
+     { 'template': 'library/privacy_policy.html' }),
     url(r'^(.*?)/?$', 'library', name="library"),
 )
 
