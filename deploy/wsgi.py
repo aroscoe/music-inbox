f084a962f56436bf601895ace8ea30ee06958e65
Anthony Roscoe <me@anthonyroscoe.com>
2010-04-10 19:39:48 -0400

    added wsgi script

---
+++ deploy/wsgi.py
@@ -0,0 +1,16 @@
+import os
+import sys
+from os.path import join
+from django.conf import settings
+
+# redirect sys.stdout to sys.stderr for bad libraries like geopy that uses
+# print statements for optional import exceptions.
+sys.stdout = sys.stderr
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'music-inbox.settings'
+
+sys.path.insert(0, join(settings.PROJECT_ROOT, "libs"))
+sys.path.insert(0, join(settings.PROJECT_ROOT, "apps"))
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
