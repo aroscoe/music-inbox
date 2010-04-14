1c63268d7d1e7b5370b3175f1e15386954064f42
Anthony Roscoe <me@anthonyroscoe.com>
2010-04-13 21:48:16 -0400

    added file type validation to upload form incase the file isn't xml. this could probably be more thorough

--- apps/library/forms.py
+++ apps/library/forms.py
@@ -3,3 +3,9 @@ from django import forms
 class UploadFileForm(forms.Form):
     name = forms.CharField(max_length=150, widget=forms.TextInput(attrs={'class': 'txtbox'}))
     file = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn_browse'}))
+    
+    def clean_file(self):
+        file = self.cleaned_data['file']
+        if file.content_type != 'text/xml':
+            raise forms.ValidationError("Wrong file type. Please choose another file.")
+        return file
