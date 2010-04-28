e871b799d9e2dba64056d603c281a1e7c21d32ce
Anthony Roscoe <me@anthonyroscoe.com>
2010-04-27 23:52:00 -0400

    started adding in jquery validation

--- apps/library/forms.py
+++ apps/library/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class UploadFileForm(forms.Form):
-    name = forms.CharField(max_length=150, widget=forms.TextInput(attrs={'class': 'txtbox'}))
+    name = forms.CharField(max_length=150, widget=forms.TextInput(attrs={'class': 'txtbox required'}))
     file = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn_browse'}))
     
     def clean_file(self):
