0fdcded1ff18a5d86b35f2d941538de711559650
Felix Berger <bflat1@gmx.net>
2010-06-01 16:48:46 -0400

    rss link in success page

--- apps/library/views.py
+++ apps/library/views.py
@@ -9,7 +9,7 @@ from django.contrib.sites.models import Site
 
 from library.models import Library, Artist
 from library import forms
-from library.utils import decrypt_id, encrypt_id
+from library import utils
 from library import tasks
 
 logging.basicConfig(filename=settings.LOG_FILE)
@@ -63,7 +63,7 @@ def upload(request):
     if request.method == 'POST':
         library, form = LibraryView().post_library(request)
         if library:
-            library_id = encrypt_id(library.pk)
+            library_id = utils.encrypt_id(library.pk)
             return redirect('library_success', library_id=library_id)
     else:
         form = forms.UploadFileForm()
@@ -71,7 +71,7 @@ def upload(request):
 
 def library(request, library_id):
     try:
-        library = Library.objects.get(pk=decrypt_id(library_id, Http404))
+        library = Library.objects.get(pk=utils.decrypt_id(library_id, Http404))
     except Library.DoesNotExist:
         raise Http404
     albums = library.albums_dict()
@@ -79,9 +79,9 @@ def library(request, library_id):
 
 def success(request, library_id):
     try:
-        library = Library.objects.get(pk=decrypt_id(library_id, Http404))
+        library = Library.objects.get(pk=utils.decrypt_id(library_id, Http404))
     except Library.DoesNotExist:
         raise Http404
-    site_domain = Site.objects.get_current().domain
+    rss_url = utils.rss_url(library.pk)
     return direct_to_template(request, 'library/success.html', locals())
 
