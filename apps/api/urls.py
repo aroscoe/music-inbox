53a4e9db45701f7592b91416adf36b5492f914c1
Anthony Roscoe <me@anthonyroscoe.com>
2010-06-12 14:02:03 -0400

    commented out all of the django_restapi code for testing

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -2,28 +2,27 @@ from django.conf.urls.defaults import *
 
 from piston.resource import Resource
 
-from api.views import LibraryResource, libraries_resource, missing
-from api.handlers import LibraryHandler, MissingLibraryHandler
+# from api.views import LibraryResource, libraries_resource, missing
+from api.handlers import LibraryHandler, LibraryFormHandler, MissingLibraryHandler
 
-library = LibraryResource(permitted_methods=('GET','POST'))
+# library = LibraryResource(permitted_methods=('GET','POST'))
 
-piston_library = Resource(handler=LibraryHandler)
-piston_form_library = Resource(handler=LibraryFormHandler)
-piston_missing_library = Resource(handler=MissingLibraryHandler)
+library = Resource(handler=LibraryHandler)
+library_form = Resource(handler=LibraryFormHandler)
+missing_library = Resource(handler=MissingLibraryHandler)
 
 urlpatterns = patterns('',
-    # Example of using django_restapi
-    url(r'^test/(.*?)/?$', libraries_resource),
+    url(r'^library/form/?$', library_form, name="api_library_form"),
+    url(r'^library/(\d+)/missing/?$', missing_library, name="api_library_missing"),
+    url(r'^library/?$', library, name="api_library_upload"),
+    url(r'^library/(?P<library_id>[^/]+)/', library, name="api_library"),
     
-    # Piston
-    url(r'^piston-library/form/?$', piston_form_library, name="piston-api_library_form"),
-    url(r'^piston-library/(\d+)/missing/?$', piston_missing_library, name="piston-api_library_missing"),
-    url(r'^piston-library/?$', piston_library, name="piston-api_library_upload"),
-    url(r'^piston-library/(?P<library_id>[^/]+)/', piston_library, name="piston-api_library"),
+    # Example of using django_restapi
+    # url(r'^test/(.*?)/?$', libraries_resource),
     
     # Library
-    url(r'^library/upload/?$', library.create, name="api_library_upload"),
-    url(r'^library/(\d+)/missing/?$', missing, name="api_library_missing"),
-    url(r'^library/form/?$', library.handle_form_data),
-    url(r'^library/(.*?)/?$', library, name="api_library"),
+    # url(r'^library/upload/?$', library.create, name="api_library_upload"),
+    # url(r'^library/(\d+)/missing/?$', missing, name="api_library_missing"),
+    # url(r'^library/form/?$', library.handle_form_data),
+    # url(r'^library/(.*?)/?$', library, name="api_library"),
 )
