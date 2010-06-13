e9f70a2f0cce171e9cb799be8977962c11d92d2f
Anthony Roscoe <me@anthonyroscoe.com>
2010-06-13 00:43:07 -0400

    added a fixture and wrote more tests for api

--- apps/api/tests.py
+++ apps/api/tests.py
@@ -2,13 +2,22 @@ from django.test import TestCase
 from django.core.urlresolvers import reverse
 
 class APILibraryTestCase(TestCase):
-    def setUp(self):
-        pass
+    fixtures = ['test_library.json']
     
-    def test_library_api(self):
+    def test_library_api_not_found(self):
         response = self.client.get(reverse('api_library', args=[1234]))
         self.assertContains(response, 'Not Found', status_code=404)
+    
+    def test_library_api_result(self):
+        response = self.client.get(reverse('api_library', args=[5013407824245992320]))
+        self.assertTrue('Jane Doe' in response.content)
+        self.assertEquals(response.status_code, 200)
         
-    def test_missing_library_api(self):
+    def test_missing_library_api_not_found(self):
         response = self.client.get(reverse('api_library_missing', args=[1234]))
         self.assertContains(response, 'Not Found', status_code=404)
+    
+    def test_missing_library_api_result(self):
+        response = self.client.get(reverse('api_library_missing', args=[5013407824245992320]))
+        self.assertTrue('No Heroes' in response.content)
+        self.assertEquals(response.status_code, 200)
