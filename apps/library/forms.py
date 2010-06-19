af3883ee5121bc909225904123b639b4136b0d3b
Anthony Roscoe <me@anthonyroscoe.com>
2010-06-19 18:40:51 -0400

    bugfix - now validating files with less than 4 lines.
    http://github.com/aroscoe/music-inbox/issues#issue/4

--- apps/library/forms.py
+++ apps/library/forms.py
@@ -8,25 +8,28 @@ class UploadFileForm(forms.Form):
     
     def clean_file(self):
         file = self.cleaned_data['file']
-        valid_content_types = ['text/xml', 'application/xml', 'application/octet-stream']
-        
-        # Check file has correct content type
-        if file.content_type in valid_content_types:
-            
-            # Read first few lines of file to double check file is what it's content type says it is
-            max_lines = 4
-            count = 1
-            pattern = '<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">'
-            for line in file:
-                if re.match(pattern, line):
-                    break
-                else:
-                    if count < 4:
-                        count += 1
-                    else:
-                        raise forms.ValidationError("Wrong file type. Please choose another file.")
-                        break
-        else:
+        if not self.validate_file_content_type(file) or not self.validate_file_contents(file):
             raise forms.ValidationError("Wrong file type. Please choose another file.")
-        
         return file
+    
+    def validate_file_content_type(self, file):
+        """Validate a file's content type with the one expected."""
+        valid_content_types = ['text/xml', 'application/xml', 'application/octet-stream']
+        if file.content_type in valid_content_types:
+            return True
+        return False
+    
+    def validate_file_contents(self, file):
+        """Read first few lines of a file checking for a specific pattern."""
+        max_lines = 4
+        count = 1
+        pattern = '<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">'
+        for line in file:
+            if re.match(pattern, line):
+                return True
+            else:
+                if count < 4:
+                    count += 1
+                else:
+                    break
+        return False
