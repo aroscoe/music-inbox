7ca0e871eb7a94d3a53d1ca9c243655360a1fddb
Anthony Roscoe <me@anthonyroscoe.com>
2010-07-05 17:58:40 -0400

    some test cleanup

--- apps/library/tests.py
+++ apps/library/tests.py
@@ -11,10 +11,10 @@ from library import utils
 from library.utils import pandora
 
 class Tests(TestCase):
-
+    
     def setUp(self):
         self.logger = logging.getLogger()
-
+    
     def test_release_group_no_releases(self):
         '''
         http://github.com/aroscoe/music-inbox/issues#issue/1
@@ -23,8 +23,7 @@ class Tests(TestCase):
         artist = MBArtist.objects.create(name = 'Galactic', mb_id = 'http://musicbrainz.org/artist/cabbdf87-5cb2-4f3c-be65-254655c87508.html')
         self.assertEquals((None, None),
                           artist.get_release_date('http://musicbrainz.org/release-group/32195cf1-5d5c-412e-b308-1d586c08e6c4.html', logger))
-
-        
+    
     def test_null_release_dates(self):
         '''
         makes sure the Library._newest_ function can accept null MBAlbum.release_date's
@@ -37,17 +36,16 @@ class Tests(TestCase):
         # null release dates
         album1 = MBAlbum.objects.create(name = 'bar', mb_id = 'http://bar.com/album1', artist = artist)
         album2 = MBAlbum.objects.create(name = 'bar', mb_id = 'http://bar.com/album2', artist = artist)
-
+        
         # non-null release date
         album3 = MBAlbum.objects.create(name = 'bar', mb_id = 'http://bar.com/album3', artist = artist, release_date = datetime.now())
         
         library = Library.objects.create(pk = 1, name = 'foo')
-
+        
         self.assertEquals(-sys.maxint, library._newest_(album1, album2))
         self.assertEquals(sys.maxint, library._newest_(album1, album3))
         self.assertEquals(-sys.maxint, library._newest_(album3, album2))
-
-
+    
     def test_fetch_albums(self):
         artist = MBArtist.objects.create(name='4 Non Blondes',
                                          mb_id='http://musicbrainz.org/artist/efef848b-63e4-4323-8ef7-69a48fbdd51d')
@@ -59,8 +57,7 @@ class Tests(TestCase):
                           album.mb_id)
         self.assertEquals(date(1992, 10, 13), album.release_date)
         self.assertEquals(artist, album.artist)
-
-
+    
     def test_get_release_date(self):
         artist = MBArtist.objects.create(name='4 Non Blondes',
                                          mb_id='http://musicbrainz.org/artist/efef848b-63e4-4323-8ef7-69a48fbdd51d')
@@ -94,13 +91,13 @@ class Tests(TestCase):
                                             mb_id='http://musicbrainz.org/artist/efef848b-63e4-4323-8ef7-69a48fbdd51d')
         library = Library.objects.create(name='test')
         artist = library.artist_set.create(name='4 Non Blondes')
-
+        
         lookup_artist(artist, self.logger)
         
         self.assertEquals(mb_artist.mb_id, artist.mb_artist_id)
         # albums should not be fetched
         self.assertEquals(0, len(MBAlbum.objects.all()))
-
+    
     def test_lookup_artist_for_new_artist_fixes_name(self):
         library = Library.objects.create(name='test')
         artist = library.artist_set.create(name='4 non blondes')
@@ -108,11 +105,11 @@ class Tests(TestCase):
         lookup_artist(artist, self.logger)
         
         self.assertEquals('4 Non Blondes', artist.name)
-
+    
     def test_missing_albums(self):
         mb_artist = MBArtist.objects.create(mb_id='http://musicbrainz.org/artist/5e521e8c-0ab2-44c4-8fd8-14d8d3321265',
                                             name='Isis')
-
+        
         celestial = MBAlbum.objects.create(mb_id='http://musicbrainz.org/release-group/e371d1e5-8737-3c79-a16e-0d4c487eedfd',
                                            artist=mb_artist,
                                            name='Celestial',
@@ -139,20 +136,20 @@ class Tests(TestCase):
                                          name='Wavering Radiant',
                                          release_date='2009-04-21',
                                          asin='B001SZ298M')
-
+        
         library = Library.objects.create(name='test')
         artist = library.artist_set.create(name='Isis')
         album = artist.album_set.create(name='Wavering Radiant')
-
+        
         lookup_artist(artist, self.logger)
-
+        
         # fetch album from db again
         album = list(artist.album_set.all())[0]
-
+        
         self.assertEquals(radiant.mb_id, album.mb_id)
-
+        
         missing_albums = library.missing_albums()
-
+        
         self.assertFalse(radiant in missing_albums)
         self.assertTrue(oceanic in missing_albums)
         self.assertTrue(absence in missing_albums)
@@ -164,18 +161,17 @@ class HttpTests(TestCase):
     
     def setUp(self):
         self.client = client.Client()
-
-
+    
     def test_post_form_and_rss(self):
         '''Uploads library data via the form api and verifies that the an rss
         uri is returned correctly. Then verifies that the new release is in the 
         rss feed.
         
         '''
-
+        
         mb_artist = MBArtist.objects.create(mb_id='http://musicbrainz.org/artist/5e521e8c-0ab2-44c4-8fd8-14d8d3321265',
                                             name='Isis')
-
+        
         celestial = MBAlbum.objects.create(mb_id='http://musicbrainz.org/release-group/e371d1e5-8737-3c79-a16e-0d4c487eedfd',
                                            artist=mb_artist,
                                            name='Celestial',
@@ -187,7 +183,7 @@ class HttpTests(TestCase):
                                          name='Oceanic',
                                          release_date='2002-09-17',
                                          asin='B00006IQHQ')
-
+        
         response = self.client.post('/api/library/form/', {'name': 'test_lib',
                                                            'Isis': 'Celestial' })
         self.assertEquals('application/json; charset=utf-8', 
@@ -200,15 +196,14 @@ class HttpTests(TestCase):
         response = self.client.get('/library/feeds/newalbums/5013407824245992320/')
         # should contain 'Oceanic' release
         self.assertTrue('Oceanic' in response.content)
-
+    
     def test_get_non_existant_rss_feed_not_found(self):
         '''Asserts that a GET request on a non existant rss feed returns 404
         error code.
-
+        
         '''
         response = self.client.get('/library/feeds/newalbums/5013407824245992320/')
         self.assertEquals(404, response.status_code)
-
 
 class PandoraTests(TestCase):
     '''Test for the pandora module.'''
