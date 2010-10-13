55fe0d4028263b02b9b9b85121a41d40cf939311
Felix Berger <bflat1@gmx.net>
2010-10-13 00:02:28 -0400

    ensure min length is >= 1

--- apps/library/utils/matching.py
+++ apps/library/utils/matching.py
@@ -9,4 +9,4 @@ def similarity(term1, term2):
     term1 = term1.lower()
     term2 = term2.lower()
     distance = Levenshtein.distance(term1, term2)
-    return 1.0 - distance / float(min(len(term1), len(term2)))
+    return 1.0 - distance / float(max(1, min(len(term1), len(term2))))
