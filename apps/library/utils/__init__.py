afc096eebf7ca95ff4eaeb6700f64c49a240f614
Felix Berger <bflat1@gmx.net>
2010-10-19 00:32:11 -0400

    utility methods to verify library is in good state

--- apps/library/utils/__init__.py
+++ apps/library/utils/__init__.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib.sites.models import Site
 
+from library import models
 import tea
 
 def decrypt_id(id, exception_to_raise):
@@ -18,3 +19,25 @@ def rss_url(library_id):
     '''Returns the full rss url for a library_id'''
     return 'http://%s/library/feeds/newalbums/%s/' % \
         (Site.objects.get_current().domain, encrypt_id(library_id))
+
+def scan_all_artists():
+    '''Iterates through all Artist objects and checks whether their mb_id
+    points to a valid MBArtist.
+    
+    '''
+    artists = models.Artist.objects.all()
+    for artist in artists:
+        if artist.mb_artist_id:
+            print artist
+            models.MBArtist.object.get(mb_id=artist.mb_artist_id)
+
+def scan_all_albums():
+    '''Iterates over all Album objects and checks whether their mb_id
+    points to a valid MBAlbum.
+
+    '''
+    albums = models.Album.objects.all()
+    for album in albums:
+        if album.mb_id:
+            print album
+            models.MBAlbum.object.get(mb_id=album.mb_id)
